6103378053ead78389948fb9201522c076d00659
margaret-ngugi <ngugiwairimumargaret@gmail.com>
2023-04-05 14:10:42 +0300

    pushing to github

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+fun main(){
+
+
+}
+fun addition(){
+    var x = 56
+    var y = 4682
+    var sum = x+y
+    println(sum)
+
+}
+fun greetMe(){
+    println("Hello,how are you today")
+}
+fun additions(num1:Int,num2:Int) {
+    var sum = num1 + num2
+    println(sum)
+}
+fun greetPerson(name:String){
+    println("Hello"+name)
+
+}
+fun addition(num1:Int,num2:Int):Int{
+    var sum = num1+num2
+    return sum
+}
+
